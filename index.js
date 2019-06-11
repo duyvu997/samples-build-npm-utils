1217c6b09391a148d224a71cff2c04d7cdf10abc
nduy79 <ngocduy0997@gmail.com>
2019-06-11 10:56:06 +0700

    commit 2: add GET-DELETE method

--- index.js
+++ index.js
@@ -26,4 +26,40 @@ server.route({
     } 
 });
 
+server.route({
+    method: "GET",
+    path:"/todo",
+    handler: async (request, h)=>{
+        try{
+            let todoJob =  await TodoModel.find().exec();
+            return h.response(todoJob);
+        }
+        catch(err){
+            return h.response(err).code(500);
+        }
+    }
+});
+
+server.route({
+    method:"PUT",
+    path:"/todo",
+    handler: async ()=>{}
+});
+
+server.route({
+    method:"DELETE",
+    path:"/todo/{name}",
+    handler: async(request, h)=>{
+        try {
+            let result =  TodoModel.findOneAndDelete({name: request.params.name}).exec(function (err, doc){
+                doc.remove();
+            });
+            return h.response(result);
+        }
+        catch(err){
+            return h.response(err).code(500);
+        }
+    }
+});
+
 server.start();
